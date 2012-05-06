7e7f40dffc92393e8fd2c3227d93c47a60f0db5c
Daniel <daniel.r.ness@gmail.com>
2012-05-06 22:07:22 +0100

    Initial dev + added license

--- zenall-mode.el
+++ zenall-mode.el
@@ -1,3 +1,22 @@
+;; -----------  zenall-mode  --------------
+;; Daniel Ness <daniel.r.ness@gmail.com>
+;; ----------------------------------------
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+;; software and associated documentation files (the "Software"), to deal in the Software 
+;; without restriction, including without limitation the rights to use, copy, modify, merge, 
+;; publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+;; to whom the Software is furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all copies or 
+;; substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+;; BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+;; DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 (setq zenall-css-lookup-alist '(
     ("o" . "outline:;")
     ("o:n" . "outline:none;")
@@ -494,11 +513,12 @@
 (defun zenall-expand ()
   (interactive)
   (save-excursion 
-    (if (re-search-backward "\\b\\(.+\\)\\b")
+    (if (re-search-backward "\\([a-z0-9:-]+\\)")
         (let* ((txt (match-string 1))
                (start (match-beginning 0))
                (end (match-end 0))
                (output (funcall zenall-lookup-fn txt)))
+          (message txt)
           (when (stringp output)
               (delete-region start end)
               (insert output))))))
@@ -511,10 +531,12 @@
 
 (add-hook 'css-mode-hook
           (lambda ()
+            (zenall-mode)
             (setq zenall-lookup-fn 'zenall-css-lookup)))
 
 (add-hook 'sgml-mode-hook
           (lambda ()
+            (zenall-mode)
             (setq zenall-lookup-fn 'zenall-sgml-lookup)))
 
 (provide 'zenall-mode)
